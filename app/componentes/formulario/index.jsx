09048796bb8f1a45be6bf801f1cde38d73ff82bb
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-22 13:34:47 +0100

    Trabajando en Formulario.css

--- app/componentes/formulario/index.jsx
+++
@@ -1,2 +0,0 @@
-export * from "./Formulario";
-export { default } from "./Formulario";
--- app/practicas/formulario/page.js
+++ app/practicas/formulario/page.js
@@ -1,5 +1,58 @@
-import Formulario from "@/app/componentes/formulario/Formulario";
+"use client";
+import styles from "./Formulario.module.css";
+import { useState } from "react";
 
-export default function Page() {
-  return <Formulario />;
+export default function Formulario() {
+  const [form, setForm] = useState({
+    nombre: "",
+    email: "",
+    mensaje: "",
+  });
+
+  const handleSubmit = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={styles.containerForm}
+    >
+      <label htmlFor="nombre">Nombre:</label>
+      <input
+        className={styles.input}
+        type="text"
+        id="nombre"
+        name="nombre"
+        value={form.nombre}
+        onChange={(e) => setForm({ ...form, nombre: e.target.value })}
+      />
+      <label htmlFor="email">Email:</label>
+      <input
+        className={styles.input}
+        type="email"
+        id="email"
+        name="email"
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+      <label htmlFor="mensaje">Mensaje:</label>
+      <textarea
+        className={styles.textarea}
+        id="mensaje"
+        name="mensaje"
+        value={form.mensaje}
+        onChange={(e) => setForm({ ...form, mensaje: e.target.value })}
+      ></textarea>
+      <button
+        className={styles.btnFormulario}
+        type="submit"
+      >
+        Enviar
+      </button>
+    </form>
+  );
 }
