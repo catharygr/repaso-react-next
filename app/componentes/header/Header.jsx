532f4d672c18023ed7ae617bd0798403d1c78208
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-22 10:54:28 +0100

    Trabajando en Header.jsx

--- app/componentes/header/Header.jsx
+++ app/componentes/header/Header.jsx
@@ -1,4 +1,13 @@
 import styles from "./Header.module.css";
+import { Home } from "react-feather";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <div className={styles.logo}>
+        <Home />
+        <h1>My Blog</h1>
+      </div>
+    </header>
+  );
 }
