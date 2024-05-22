220163beb818647278081a415ba3bb732883df41
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-22 12:30:22 +0100

    Trabajando en Formulario.jsx

--- app/componentes/formulario/Formulario.jsx
+++ app/componentes/formulario/Formulario.jsx
@@ -1,14 +1,32 @@
 import styles from "./Formulario.module.css";
 
 export default function Formulario() {
+  const [form, setForm] = useState({
+    nombre: "",
+    email: "",
+    mensaje: "",
+  });
+
+  const handleSubmit = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
   return (
-    <form className={styles.containerForm}>
+    <form
+      onSubmit={handleSubmit}
+      className={styles.containerForm}
+    >
       <label htmlFor="nombre">Nombre:</label>
       <input
         className={styles.input}
         type="text"
         id="nombre"
         name="nombre"
+        value={form.nombre}
+        onChange={(e) => setForm({ ...form, nombre: e.target })}
       />
       <label htmlFor="email">Email:</label>
       <input
@@ -16,12 +34,16 @@ export default function Formulario() {
         type="email"
         id="email"
         name="email"
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target })}
       />
       <label htmlFor="mensaje">Mensaje:</label>
       <textarea
         className={styles.textarea}
         id="mensaje"
         name="mensaje"
+        value={form.mensaje}
+        onChange={(e) => setForm({ ...form, mensaje: e.target })}
       ></textarea>
       <button
         className={styles.btnFormulario}
