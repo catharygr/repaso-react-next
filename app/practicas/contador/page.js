48c854822e1c3fef032aadaaa7f2830b7c3d20eb
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-23 13:55:36 +0100

    Trabajando en Contador.jsx

--- app/practicas/contador/page.js
+++ app/practicas/contador/page.js
@@ -5,14 +5,25 @@ import styles from "./contador.module.css";
 export default function Contador() {
   const [contador, setContador] = useState(0);
 
+  const handleClick = () => {
+    setContador(contador + 1);
+    setContador(contador - 1);
+  };
+
   return (
     <div className={styles.containerContador}>
       <p className={styles.p}>{contador}</p>
       <button
         className={styles.btnContador}
-        onClick={() => setContador(contador + 1)}
+        onClick={() => handleClick(contador + 1)}
       >
         Incrementar
+      </button>
+      <button
+        className={styles.btnContador}
+        onClick={() => handleClick(contador - 1)}
+      >
+        Decrementar
       </button>
     </div>
   );
