01aefe917cceced90a9c15c357cf8762d5a2300f
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-23 13:38:58 +0100

    Trabajando en el botón del contador

--- app/practicas/contador/page.js
+++ app/practicas/contador/page.js
@@ -1,11 +1,14 @@
 "use client";
+import { useState } from "react";
+import styles from "./contador.module.css";
 
 export default function Contador() {
   const [contador, setContador] = useState(0);
+
   return (
-    <>
-      <p>Contador</p>
-      <button on>Incrementar</button>
-    </>
+    <div className={styles.containerContador}>
+      <p className="">{contador}</p>
+      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+    </div>
   );
 }
