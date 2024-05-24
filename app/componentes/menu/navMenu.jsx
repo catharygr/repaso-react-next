e7354ecd55e417b62d0824682d74a9d8b6d960f2
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-24 14:19:31 +0100

    Trabajando en el componente NavMenu.jsx

--- app/componentes/menu/navMenu.jsx
+++ app/componentes/menu/navMenu.jsx
@@ -1,5 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
 import styles from "./NavMenu.module.css";
+import { useId, useState } from "react";
+
 export default function NavMenu() {
+  const [menuHamburguer, setMenuHamburguer] = useState(null);
+  const pathname = usePathname;
+  const id = useId();
+
   return (
     <nav>
       <ul>
