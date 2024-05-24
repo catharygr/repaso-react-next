59d658e0efb89660384a9a7821ef876231791ca8
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-24 14:41:18 +0100

    Trabajando en el componente NavMenu.jsx

--- app/componentes/menu/navMenu.jsx
+++ app/componentes/menu/navMenu.jsx
@@ -3,10 +3,29 @@ import { usePathname } from "next/navigation";
 import styles from "./NavMenu.module.css";
 import { useId, useState } from "react";
 
-export default function NavMenu() {
+export default function NavMenu({ setMenuLink }) {
   const [menuHamburguer, setMenuHamburguer] = useState(null);
   const pathname = usePathname;
   const id = useId();
+
+  useEffect(() => {
+    const elementoEnfocadoAntesDeAbrirlo = document.activeElement;
+    return () => {
+      elementoEnfocadoAntesDeAbrirlo?.focus();
+    };
+  }, []);
+
+  useEffect(() => {
+    function handleEscape(e) {
+      if (e.key === "Escape") {
+        setMenuLink();
+      }
+    }
+    window.addEventListener("keydown", handleEscape);
+    return () => {
+      window.removeEventListener("keydown", handleEscape);
+    };
+  }, [setMenuLink]);
 
   return (
     <nav>
