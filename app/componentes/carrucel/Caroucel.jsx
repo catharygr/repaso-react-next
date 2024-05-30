dcb201e0f28b65fbfae662a953b5d226a2e9df2c
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-30 11:53:02 +0100

    Trabajando en el componente Caroucel.jsx

--- app/componentes/carrucel/Caroucel.jsx
+++ app/componentes/carrucel/Caroucel.jsx
@@ -1,7 +1,21 @@
+import styles from "./Caroucel.module.css";
+
 export default function Caroucel() {
   return (
-    <div>
-      <h1>Caroucel</h1>
-    </div>
+    <section className={styles.containerCaroucel}>
+      <div className={styles.containerColor}>
+        <div className={styles.contenido}>
+          <h2>Todos los conocimientos</h2>
+          <h3>Herramientas</h3>
+          <p>
+            ¡Hola! Iniciada en el mundo de la programación como junior, con
+            muchas ganas de llevar a cabo todo lo aprendido en diferentes
+            bootcamps, como en las plataformas interactivas: Qualentum, Josh W
+            Comeau's, Practical Accessibility con Sara Soueidan, Platzi,
+            Scrimba, FreeCodeCamp.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
