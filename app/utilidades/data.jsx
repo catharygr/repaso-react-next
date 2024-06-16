7c5b2fd72003b5d2ab9156bf534d2421c9a93192
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-16 11:52:43 +0100

    Trabajando en data.jsx

---
+++ app/utilidades/data.jsx
@@ -0,0 +1,12 @@
+export const hamburguer_link = [
+  {
+    slug: "formulario",
+    text: "Formulario",
+    href: "/practicas/formulario",
+  },
+  {
+    slug: "contador",
+    text: "Contador",
+    href: "/practicas/contador",
+  },
+];
