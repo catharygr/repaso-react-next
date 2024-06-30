cec7b70df46bc6dc0b2e56417fe99c956ac64e56
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-30 15:21:49 +0100

    Trabajando en el componente Carousel.jsx

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className={styles.h1}>Mis prácticas de React con Nextjs</h1>
+      <Caroucel />
     </main>
   );
 }
