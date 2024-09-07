869176de710a6ac9b0456a3134dea68cd411e83d
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-07 18:12:18 +0100

    Trabajando en carousel.module.css

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import Caroucel from "./componentes/carousel/Carousel";
+import Carousel from "./componentes/carousel/Carousel";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className={styles.h1}>Mis prácticas de React con Nextjs</h1>
-      <Caroucel />
+      <Carousel />
     </main>
   );
 }
