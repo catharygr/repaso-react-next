6370d7046690a8a06989a83075980ca968d01322
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-08 15:00:10 +0100

    Trabajando em profesionalidad.module.css

--- app/componentes/profesionalidad/Profesionalidad.jsx
+++ app/componentes/profesionalidad/Profesionalidad.jsx
@@ -6,13 +6,15 @@ export function Profesionalidad() {
   return (
     <div className={styles.containerExterior}>
       <div className={styles.container}>
-        <h1>Profesionalidad:</h1>
-        <p>
-          La profesionalidad es un valor que se adquiere con el tiempo y la
-          experiencia. Es la capacidad de desempeñar un trabajo de forma
-          correcta y eficiente, cumpliendo con las expectativas y necesidades
-          del cliente.
-        </p>
+        <div className={styles.contenido}>
+          <h1>Profesionalidad:</h1>
+          <p>
+            La profesionalidad es un valor que se adquiere con el tiempo y la
+            experiencia. Es la capacidad de desempeñar un trabajo de forma
+            correcta y eficiente, cumpliendo con las expectativas y necesidades
+            del cliente.
+          </p>
+        </div>
         <div className={styles.PlantaUno}>
           <Image
             src={PlantaUno}
