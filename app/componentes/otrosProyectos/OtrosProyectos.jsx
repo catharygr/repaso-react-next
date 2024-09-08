21ea9ee4932887b53177062953fa1d05064c13b9
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-08 12:45:46 +0100

    Trabajando con OtrosProyectos.module.css

--- app/componentes/otrosProyectos/OtrosProyectos.jsx
+++ app/componentes/otrosProyectos/OtrosProyectos.jsx
@@ -15,7 +15,12 @@ export function OtrosProyectos() {
             aplicación web que te permite conocer los lugares más emblemáticos
             de Sarajevo.
           </p>
-          <Link href="https://sarajevo-turismo.netlify.app">Ver proyecto</Link>
+          <Link
+            className={styles.contenidoLink}
+            href="https://sarajevo-turismo.netlify.app"
+          >
+            Ver proyecto
+          </Link>
         </div>
         <Image
           className={styles.imagenChicaDos}
@@ -30,7 +35,10 @@ export function OtrosProyectos() {
             E-commerce es una aplicación web online que permite hacer compras
             sin tener que visitar una tienda física.
           </p>
-          <Link href="https://opencells-ecommerce-lit-caty.netlify.app">
+          <Link
+            className={styles.contenidoLink}
+            href="https://opencells-ecommerce-lit-caty.netlify.app"
+          >
             Ver proyecto
           </Link>
         </div>
