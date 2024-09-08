771ee445418d32549b8753d0add09fda74eddeab
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-08 10:59:29 +0100

    Trabajando en el componente OtrosProyectos.jsx

---
+++ app/componentes/otrosProyectos/OtrosProyectos.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-feather";
+
+export function OtrosProyectos() {
+  return (
+    <aside>
+      <div>
+        <h2>Proyectos</h2>
+        <p>Descripción</p>
+        <Link href="#">Ver proyecto</Link>
+      </div>
+      <img />
+      <div>
+        <h2>Proyectos</h2>
+        <p>Descripción</p>
+        <Link href="#">Ver proyecto</Link>
+      </div>
+    </aside>
+  );
+}
