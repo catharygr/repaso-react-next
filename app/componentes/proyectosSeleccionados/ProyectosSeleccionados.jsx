1746fd3c5c835cbf714e81e24fca31af6f968118
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-09 14:52:57 +0100

    Trabajando en CardProyecto.module.css

--- app/componentes/proyectosSeleccionados/ProyectosSeleccionados.jsx
+++ app/componentes/proyectosSeleccionados/ProyectosSeleccionados.jsx
@@ -4,10 +4,10 @@ import CardProyecto from "../card";
 export function ProyectosSeleccionados({ contenido }) {
   console.log(contenido[0]);
   return (
-    <section className={styles.containerExterior}>
+    <section className={styles.proyectoWrapper}>
       <h1 className={styles.h1}>Proyectos Seleccionados</h1>
       <div className={styles.containerInterior}>
-        <div className="proyecto">
+        <div className={styles.cardsContainer}>
           {contenido.slice(0, 3).map((proyecto) => (
             <CardProyecto
               key={proyecto.id}
@@ -19,18 +19,4 @@ export function ProyectosSeleccionados({ contenido }) {
       <button className={styles.boton}>Todos los proyectos </button>
     </section>
   );
-}
-
-{
-  /* <div className={`wrapper flow ${styles.proyectoWrapper}`}>
-<h2 className={styles.h2}>Últimos Proyectos</h2>
-<div className={styles.cardsContainer}>
-  {ultimosProyectos.map((proyecto) => (
-    <Card
-      key={proyecto.url}
-      content={proyecto}
-      type="conImg"
-    />
-  ))}
-</div> */
 }
