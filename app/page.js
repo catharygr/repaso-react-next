9a3a920d4f89021ab31b0e2c70121de5a2cc504f
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-09 11:15:38 +0100

    Trabajando en la imagen en page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import Carousel from "./componentes/carousel/Carousel";
 import { OtrosProyectos } from "./componentes/otrosProyectos/OtrosProyectos";
 import { Profesionalidad } from "./componentes/profesionalidad/Profesionalidad";
+import Image from "next/image";
+import Regadera from "../public/imagen/regadera.svg";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.h1}>Mis prácticas de React con Nextjs</h1>
+    <main
+      style={{
+        backgroundImage: `url(${Regadera.src})`,
+      }}
+      className={styles.main}
+    >
+      <h1 className={styles.h1}>Prácticas de React con Nextjs</h1>
       <Carousel />
       <OtrosProyectos />
       <Profesionalidad />
