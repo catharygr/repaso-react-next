b7af9b74e83ceaddc1f078f927a6916254309093
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-09 16:13:40 +0100

    Trabajando en CardProyecto.module.css

--- app/componentes/card/CardProyecto.jsx
+++ app/componentes/card/CardProyecto.jsx
@@ -5,17 +5,15 @@ export default function CardProyecto({ contenido }) {
   const { titulo, descripcion, img } = contenido;
   console.log(contenido);
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <Image
-          src={img}
-          alt="Imagen del proyecto"
-          className={styles.cardImg}
-        />
-        <div className={styles.cardContainer}>
-          <h2>{titulo}</h2>
-          <p>{descripcion}</p>
-        </div>
+    <div className={styles.card}>
+      <Image
+        src={img}
+        alt="Imagen del proyecto"
+        className={styles.cardImg}
+      />
+      <div className={styles.cardContainer}>
+        <h2>{titulo}</h2>
+        <p>{descripcion}</p>
       </div>
     </div>
   );
--- app/componentes/proyectosSeleccionados/ProyectosSeleccionados.jsx
+++ app/componentes/proyectosSeleccionados/ProyectosSeleccionados.jsx
@@ -6,15 +6,14 @@ export function ProyectosSeleccionados({ contenido }) {
   return (
     <section className={styles.proyectoWrapper}>
       <h1 className={styles.h1}>Proyectos Seleccionados</h1>
-      <div className={styles.containerInterior}>
-        <div className={styles.cardsContainer}>
-          {contenido.slice(0, 3).map((proyecto) => (
-            <CardProyecto
-              key={proyecto.id}
-              contenido={proyecto}
-            />
-          ))}
-        </div>
+
+      <div className={styles.cardsContainer}>
+        {contenido.slice(0, 3).map((proyecto) => (
+          <CardProyecto
+            key={proyecto.id}
+            contenido={proyecto}
+          />
+        ))}
       </div>
       <button className={styles.boton}>Todos los proyectos </button>
     </section>
