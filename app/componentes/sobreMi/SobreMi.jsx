48bf1d85d6b0c77e7b953dabc9ecce29e14223b1
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-10 13:00:09 +0100

    Trabajando con el componente SobreMi.jsx

--- app/componentes/sobreMi/SobreMi.jsx
+++ app/componentes/sobreMi/SobreMi.jsx
@@ -2,7 +2,7 @@ import styles from "./SobreMi.module.css";
 export function SobreMi() {
   return (
     <div>
-      <h1>Por qué a mi</h1>
+      <h1>¿Por qué a mi?</h1>
       <p>Texto sobre mi</p>
     </div>
   );
