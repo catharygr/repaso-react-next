4041e22840e4249eb2358dc53794d89ac41ff092
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-11 14:25:36 +0100

    Trabajando en componente MiMapa.jsx

--- app/componentes/miLocalizacion/MiMapa.jsx
+++ app/componentes/miLocalizacion/MiMapa.jsx
@@ -1,7 +1,21 @@
+import styles from "./MiMapa.module.css";
+import Image from "next/image";
+import Mapa from "../../../public/imagen/mapa.svg";
 export default function MiMapa() {
   return (
-    <div>
-      <h1>Mapa</h1>
-    </div>
+    <section className={styles.container}>
+      <div className={styles.contenido}>
+        <h1>EL mapa</h1>
+        <p>Mi ubicación</p>
+        <p>Las Palmas de G.C.</p>
+        <p>Teléfono: 666 666 666</p>
+
+        <button>Abrir en Apple maps</button>
+        <button>Abrir en Google maps</button>
+      </div>
+      <div>
+        <Image src={Mapa} />
+      </div>
+    </section>
   );
 }
