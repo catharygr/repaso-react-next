49f51a551c29a0cfbf0ce4128ddfca0a56cbfc65
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-13 15:54:49 +0100

    Trabajando en componente MiMapa.jsx

--- app/componentes/miLocalizacion/MiMapa.jsx
+++ app/componentes/miLocalizacion/MiMapa.jsx
@@ -6,8 +6,7 @@ export default function MiMapa() {
     <div className={styles.container}>
       <div className={styles.contenido}>
         <h1 className={styles.h1}>EL mapa</h1>
-        <p>Mi ubicación</p>
-        <p>Las Palmas de G.C.</p>
+        <p className="">Mi ubicación: Las Palmas de Gran Canaria.</p>
         <p>Teléfono: 666 666 666</p>
 
         <button>Abrir en Apple maps</button>
