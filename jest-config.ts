2b4aff5aa6f566a55d71ffa5f911797b9f585e08
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-16 11:41:45 +0100

    Trabajando en page.test.jsx

--- jest-config.ts
+++ jest-config.ts
@@ -11,6 +11,7 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
+
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
