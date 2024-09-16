c3040b1eb36d2c38b1ad44cdd23c822f7297d122
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-16 13:56:59 +0100

    Trabajando en page.test.jsx

--- __tests__/page.test.jsx
+++ __tests__/page.test.jsx
@@ -1,11 +1,15 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
-import Carousel from "../app/componentes/carousel/Carousel";
-// import Formulario from "../app/practicas/formulario";
+import Formulario from "../app/practicas/formulario";
 
-test(" renders Cualquier pregunta aquí p", () => {
-  render(<Carousel />);
-  const linkElement = screen.getByText(/Cualquier pregunta aquí/i);
-  expect(linkElement).toBeInTheDocument();
+test("focus and blur input", () => {
+  render(<Formulario />);
+  const input = screen.getByTestId("element-to-focus");
+
+  input.focus();
+  expect(input).toHaveFocus();
+
+  input.blur();
+  expect(input).not.toHaveFocus();
 });
