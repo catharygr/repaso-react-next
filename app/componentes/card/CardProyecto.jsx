9e2cd674eb8f64297b14a2021f21044574779e84
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-17 10:43:19 +0100

    Trabajando en CardProyecto.jsx

--- app/componentes/card/CardProyecto.jsx
+++ app/componentes/card/CardProyecto.jsx
@@ -1,22 +1,28 @@
 import Image from "next/image";
 import styles from "./CardProyecto.module.css";
+import Link from "next/link";
 
 export default function CardProyecto({ contenido }) {
   const { titulo, descripcion, img, url } = contenido;
 
   return (
     <div className={styles.container}>
-      <div className={styles.card}>
-        <Image
-          src={img}
-          alt="Imagen del proyecto"
-          className={styles.cardImg}
-        />
-        <div className={styles.cardContainer}>
-          <h2 className={styles.h2}>{titulo}</h2>
-          <p>{descripcion}</p>
+      <Link
+        href={url}
+        className={linkCard}
+      >
+        <div className={styles.card}>
+          <Image
+            src={img}
+            alt="Imagen del proyecto"
+            className={styles.cardImg}
+          />
+          <div className={styles.cardContainer}>
+            <h2 className={styles.h2}>{titulo}</h2>
+            <p>{descripcion}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
