4de9c9d36d9967add8763fb4571763f574dd89cb
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-18 13:40:26 +0100

    Trabajando en portafolio.module.css

--- app/paginas-proyectos/portafolio/page.jsx
+++ app/paginas-proyectos/portafolio/page.jsx
@@ -1,11 +1,17 @@
 import styles from "./portafolio.module.css";
 
 export default function Portafolio() {
+  const today = new Date().toLocaleDateString("es-ES", {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+  });
+
   return (
     <section className={styles.container}>
       <div className={styles.img}>
         <h1 className={styles.h1}>Portafolio personal</h1>
-        <p>
+        <p className={styles.p}>
           Mi Proyecto Portafolio es como mi tarjeta de presentación digital, un
           rincón donde puedo mostrar al mundo mis creaciones y mi viaje en el
           universo de la programación. Antes de esta página web, habia
@@ -17,6 +23,14 @@ export default function Portafolio() {
           un proyecto, ¿Por qué no desafiar un poco las normas y mostrar lo que
           he logrado?
         </p>
+        <div className={styles.infoStrong}>
+          <p>
+            <strong>Publicación: 24/1/2024</strong>
+          </p>
+          <p>
+            <strong>Actualización: {today}</strong>
+          </p>
+        </div>
       </div>
     </section>
   );
