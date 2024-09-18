ea1c26776be92365eff8fe11a82f59a3281aee28
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-18 12:57:28 +0100

    importada la letra mona-sanz desde localFont

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,24 @@
 import "./globals.css";
 import Header from "./componentes/header";
 import Footer from "./componentes/footer/Footer";
+import localFont from "next/font/local";
 
 export const metadata = {
   title: "Repaso de Next.js",
   description: "Repasando Nextjs con React",
 };
 
+const monaSanzFont = localFont({
+  src: "./assets/Mona-Sans.woff2",
+  display: "swap",
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html
+      lang="es"
+      className={monaSanzFont.className}
+    >
       <body>
         <Header />
         {children}
