7101bbca7ca3d63c24ef29f0fcf7b6a7ab0c11fc
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-22 13:36:52 +0100

    Trabajando z-index.module.css

--- app/practicas/z-index/page.js
+++ app/practicas/z-index/page.js
@@ -1,10 +1,10 @@
-import { Divide } from "react-feather";
+import styles from "./z-index.module.css";
 
 export default function Zindex() {
   return (
-    <div>
-      <h1>Z-index</h1>
-      <div>
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Z-index</h1>
+      <div className={styles.zIndex}>
         <p>Hola</p>
         <p>Como estas</p>
         <p>Estoy bien</p>
