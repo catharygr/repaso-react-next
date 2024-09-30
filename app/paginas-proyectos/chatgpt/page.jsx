38b3995c50931c91d378d0e255b04fad066e8c41
catharygr <103951447+catharygr@users.noreply.github.com>
2024-09-30 15:19:36 +0100

    Trabajando en chtgpt.module.jsx

--- app/paginas-proyectos/chatgpt/page.jsx
+++ app/paginas-proyectos/chatgpt/page.jsx
@@ -14,16 +14,9 @@ export default function ChatGPT() {
       <div className={styles.contenido}>
         <h1 className={styles.h1}>ChatGPT - App</h1>
         <p className={styles.p}>
-          Mi Proyecto Portafolio es como mi tarjeta de presentación digital, un
-          rincón donde puedo mostrar al mundo mis creaciones y mi viaje en el
-          universo de la programación. Antes de esta página web, habia
-          desarrollado otra con HTML, CSS y JavaScript. Al terminar las
-          enseñanzas de este último bootcamp, he decidido construir una nueva
-          plataforma utilizando React y Next.js. Aquí muestro con orgullo cada
-          proyecto que he creado a lo largo de tres años de constante
-          aprendizaje. Aunque no se recomienda incluir el propio portafolio como
-          un proyecto, ¿Por qué no desafiar un poco las normas y mostrar lo que
-          he logrado?
+          Conexión con la API de OpenAI para crear un chatbot. Firebase RealTime
+          Database para Back-end y Auth. Netlify env variable para ocultar API
+          key. Netlify función para conectar con OpeAI y obtener las respuestas.
         </p>
         <div className={styles.infoStrong}>
           <p>
