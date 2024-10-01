9583888918eefe3a17efb052bcfd2b1a389b352b
catharygr <103951447+catharygr@users.noreply.github.com>
2024-10-01 12:46:15 +0100

    Trabajando con la libreria framer-motion

--- app/componentes/card/CardProyectoSmall.jsx
+++ app/componentes/card/CardProyectoSmall.jsx
@@ -1,4 +1,5 @@
 import styles from "./CardProyectoSmall.module.css";
+
 import Link from "next/link";
 
 export default function CardProyectoSmall({ contenido }) {
--- app/componentes/proyectosSeleccionados/ProyectosSeleccionados.jsx
+++ app/componentes/proyectosSeleccionados/ProyectosSeleccionados.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import styles from "./ProyectosSeleccionados.module.css";
 import CardProyecto from "../card";
 import CardProyectoSmall from "../card/CardProyectoSmall";
+import { motion } from "framer-motion";
 
 export default function ProyectosSeleccionados({ contenido }) {
   const [isAllProyectos, setIsAllProyectos] = useState(false);
@@ -21,6 +22,14 @@ export default function ProyectosSeleccionados({ contenido }) {
             contenido={proyecto}
           />
         ))}
+      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        exit={{ opacity: 0 }}
+        className={styles.cardsContainer}
+      >
         {isAllProyectos &&
           contenido.slice(3, 6).map((proyecto) => (
             <CardProyectoSmall
@@ -28,7 +37,7 @@ export default function ProyectosSeleccionados({ contenido }) {
               contenido={proyecto}
             />
           ))}
-      </div>
+      </motion.div>
       <button
         onClick={handleClick}
         className={styles.boton}
